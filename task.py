39672943d21b838c53056fa07ca3a84021c42e94
Henrique Fan <fanhenrique@gmail.com>
2023-04-18 13:27:45 -0300

    task using colored

--- task.py
+++ task.py
@@ -1,10 +1,7 @@
 import datetime as dt
 import termcolor as tc
 
-from colored import colored_id
-
-priority_color = {0:'light_grey', 1:'light_yellow', 2:'light_red'}
-priority_attrs = {0:[], 1:['bold'], 2:['bold', 'underline', 'blink']}
+import colored
 
 class Task():
   def __init__(self, id, text, check=False, started=False, star=False, priority=0):
@@ -17,14 +14,12 @@ class Task():
     self.date = dt.datetime.now().timestamp()
   
 
-  def __str__(self):
-    return colored_id(self.id) + tc.colored(
-        text='\u2714 ' if self.check else'\u25A1 ',
-        color='light_green' if self.check else priority_color[self.priority],
-        attrs=[],
-        ) + tc.colored(
-          self.text,
-          color=priority_color[self.priority],
-          attrs=priority_attrs[self.priority],
-        )
+  def __str__(self, level):
+    return (
+      colored.pipe(level=level) +
+      colored.id(id=self.id) + 
+      colored.task(text=self.text, check=self.check, started=self.started, priority=self.priority) +
+      colored.date(date=self.date) + 
+      colored.star(star=self.star)
+    )
       
