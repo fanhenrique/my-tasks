7c7723fa6f71075e9a2a58ff70e7da42e9289c15
Henrique Fan <fanhenrique@gmail.com>
2023-04-20 14:34:16 -0300

    update colored

--- colored.py
+++ colored.py
@@ -41,7 +41,7 @@ def date(date):
 
 def star(star=False):
   return tc.colored(
-      text=' \u2B50' if star else '',
+      text=' \u2605' if star else '',
       color='light_yellow',
     )
 
@@ -49,43 +49,102 @@ def board(text):
   return (
     tc.colored(
       text='#',
-      color='light_green',
+      color='green',
       attrs=['bold'],
     ) + 
     tc.colored(
       text=text,
-      color='light_green',
+      color='green',
       attrs=['bold','underline']
     )
   )
 
-priority_color = {0:'light_grey', 1:'light_yellow', 2:'light_red'}
-priority_attrs = {0:[], 1:['bold'], 2:['bold', 'underline', 'blink']}
+priority_color = {0:'white', 1:'light_yellow', 2:'light_red'}
+priority_attrs = {0:['bold'], 1:['bold'], 2:['bold', 'underline']}
 
 def task(text, check=False, started=False, priority=0):
   return (
     tc.colored(
-      text='\u2714 ' if check else '\u2026 ' if started else '\u25A1 ',
-      color='light_green' if check else priority_color[priority],
-      attrs=[],
-    ) + 
+      text='\u2713 ' if check else '\u25A3 ' if started else '\u25A1 ',# '\u22EF ' '\u22C5'*2 '\u0387'*2 '\u2812 ',
+      color='light_green' if check else 'blue' if started else priority_color[priority],
+      attrs= []
+    ) + #tc.colored(text='aa', color='dark_grey', attrs=[]) +
     tc.colored(
-      text=text,
-      color=priority_color[priority],
-      attrs=priority_attrs[priority],
+      text=text,#''.join([f'\u0336{c}' for c in text]) if check else text,
+      color='light_grey' if check else priority_color[priority],
+      attrs=['dark'] if check else priority_attrs[priority],
     )
   )
 
 def note(text):
   return (
     tc.colored(
-      text='\u25CF ',
-      color='light_blue',
+      text='\u25C9 ',#'\u25C8 '
+      color='magenta',
       attrs=[],
     ) + 
     tc.colored(
       text=text,
-      color='light_grey',
+      color='magenta',
       attrs=[],
     )
+  )
+
+def all_tree_info(complete, done, pending, started, not_started, notes):
+
+  line1 = f'\n{complete:.2f}% of all tasks complete.\n'
+  d = f'{done} '
+  p = f'{pending} '
+  s = f'{started} '
+  ns = f'{not_started} '
+  n = f'{notes} '
+
+  return (
+    tc.colored(
+      text=line1,
+      color='light_grey',
+      attrs=['dark'],
+    ) + 
+    tc.colored(
+      text='\u2713 '+d,
+      color='light_green',
+      attrs=['bold'],
+    ) + tc.colored(text='done \u2223 ', color='light_grey', attrs=['dark', 'bold']) +
+    tc.colored(
+      text=p,
+      color='white',
+      attrs=['bold'],
+    ) + tc.colored(text='pending ', color='light_grey', attrs=['dark', 'bold']) +
+    tc.colored(
+      text='\u25A3 '+s,
+      color='blue',
+      attrs=['bold'],
+    ) + tc.colored(text='started ', color='light_grey', attrs=['dark', 'bold']) +
+    tc.colored(
+      text='\u25A1 ',
+      color=priority_color[0],
+      attrs=[],
+    ) +
+    tc.colored(
+      text='\u25A1 ',
+      color=priority_color[1],
+      attrs=[],
+    ) +
+    tc.colored(
+      text='\u25A1 ',
+      color=priority_color[2],
+      attrs=[],
+    ) +
+    tc.colored(
+      text=ns,
+      color='white',
+      attrs=['bold'],
+    ) + tc.colored(text='not started \u2223 ', color='light_grey', attrs=['dark', 'bold']) +
+    tc.colored(
+      text='\u25C9 '+n,
+      color='magenta',
+      attrs=['bold'],
+    ) + tc.colored(text='notes', color='light_grey', attrs=['dark', 'bold'])
+
+
   )
