1a35272777142a8058205484bf5fdd7d94d7d471
Henrique Fan <fanhenrique@gmail.com>
2023-05-10 16:40:49 -0300

    hash timeline

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from tree.note import Note
 from tree.task import Task
 from tree.board import Board
 
+count = 1
+
 def count_date(date):
     diference = dt.datetime.now().timestamp() - date
     
@@ -40,3 +42,17 @@ def find_missing(x):
   difference = list(set(range(x[0], x[-1]+1)).difference(x))
   
   return difference[0] if len(difference) else x[-1]+1
+
+
+def hash_timeline(hash_timeline, date):
+  
+  global count
+  
+  value = hash_timeline.get(date)
+
+  if value is None:
+    value = count
+    hash_timeline[date] = count
+    count += 1
+
+  return value
