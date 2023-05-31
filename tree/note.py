1b30201fa9f5a12c3e3ac8310c85f4b3604ae923
fanhenrique <fanhenrique@gmail.com>
2023-05-31 11:37:51 -0300

    node is the parent of the note

--- tree/note.py
+++ tree/note.py
@@ -2,14 +2,9 @@ import datetime as dt
 
 import colored as colored
 
-class Note():
+from .node import Node
 
-  def __init__(self, id:int, text:str, star:bool=False, date:float=None):
-    self.id = id
-    self.text = text
-    self.star = star
-    self.date = date if date else dt.datetime.now().timestamp()
-
+class Note(Node):
 
   def __str__(self, level:int=0 , show_date:bool=False):
     return (
@@ -18,15 +13,3 @@ class Note():
       (colored.date(date=self.date) if show_date else '') +
       (colored.star() if self.star else '')
     )
-
-
-  def change_star(self):
-    self.star = not self.star
-    return self.star
-
-
-  def change_text(self, text:str):
-    self.text = text
-    
-
-      
