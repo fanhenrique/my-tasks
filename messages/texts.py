94a23f23f23049ac7408ce576ae0552e08186f36
Henrique Fan <fanhenrique@gmail.com>
2023-06-02 18:09:18 -0300

    update texts

--- messages/texts.py
+++ messages/texts.py
@@ -1,6 +1,5 @@
 # icons 
-# other icons interesting
-#'\u22EF' '\u22C5'*2 '\u0387'*2 '\u2812'
+# other icons interesting '\u22EF' '\u22C5'*2 '\u0387'*2 '\u2812'
 PIPE_ICON = '\u2223'
 STAR_ICON = '\u2605'
 CHECK_ICON = '\u2713'
@@ -16,39 +15,41 @@ PENDING = 'pending'
 NOTES = 'notes'
 
 ID = 'ID'
-SUCCESS = 'SUCCESS'
-ERROR = 'ERROR'
 NOT_VALID = 'not valid'
-DELETED = 'deleted'
-CREATED = 'created'
-
-# cannot_add_new_node
-CANNOT_ADD_A_NEW_NODE_IN_A_TASK = 'Cannot add a new node in a task'
-CANNOT_ADD_A_NEW_NODE_IN_A_NOTE = 'Cannot add a new node in a note'
-
-# id not found
 NOT_FOUND = 'not found'
 NODE_ID = f'Node {ID}'
 
-# board not found per name
+# messages board not found per name
 NODE_WITH = 'Node with'
 NAME_NOT_FOUND = f'name {NOT_FOUND}'
 
-# messagens about the tree 
-TREE_EMPTY = 'Tree is empty'
-NEW_TREE_CREATE_IN = 'New tree create in'
-THE_FIRST_NODE_MUST_BE_A_BOARD = 'The first node must be a board'
+# operations confirmation messages
+SUCCESS = 'SUCCESS'
+ERROR = 'ERROR'
 
-# messagens about the task
+# messages about the node
+DELETED = 'deleted'
+CREATED = 'created'
+EDITED = 'edited'
+STAR = 'star'
+NOT_STAR = 'not star'
+
+# messages about the task
 STARTED = 'started'
 NOT_STARTED = 'not started'
 CHECK = 'check'
 NOT_CHECK = 'not_check'
-STAR = 'star'
-NOT_STAR = 'not star'
-EDITED = 'edited'
 CHANGED_PRIORITY = 'changed priority'
 ONLY_TASKS_HAVE_PRIORITY = 'Only tasks have priority'
 ONLY_TASKS_CAN_BE_STARTED = 'Only tasks can be started'
 ONLY_TASKS_CAN_BE_CHECKED = 'Only tasks can be checked'
 PRIORITY_LEVEL_OUT_OF_RANGE = 'Priority level out of range'
+
+# messages cannot add new node
+CANNOT_ADD_A_NEW_NODE_IN_A_TASK = 'Cannot add a new node in a task'
+CANNOT_ADD_A_NEW_NODE_IN_A_NOTE = 'Cannot add a new node in a note'
+
+# messages about the tree 
+TREE_EMPTY = 'Tree is empty'
+NEW_TREE_CREATE_IN = 'New tree create in'
+THE_FIRST_NODE_MUST_BE_A_BOARD = 'The first node must be a board'
