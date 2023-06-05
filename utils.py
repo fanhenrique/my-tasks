95d53a12912c597d4e1642f758db28213b49cdd0
Henrique Fan <fanhenrique@gmail.com>
2023-06-05 20:45:43 -0300

    string_type_node function not necessary

--- utils.py
+++ utils.py
@@ -1,27 +1,13 @@
-import datetime as dt
-
-from tree.note import Note
-from tree.task import Task
-from tree.board import Board
-
 HEADER_CSV = ('node', 'id', 'date', 'star', 'text', 'children', 'check', 'started', 'priority')
 
-
-def string_type_node(node, first_upcase=False):
-  if isinstance(node, Note):
-    return 'Note' if first_upcase else 'note'
-  elif isinstance(node, Task):
-    return 'Task' if first_upcase else 'task'
-  elif isinstance(node, Board):
-    return 'Board' if first_upcase else 'board'
-
-
-def all_args_are_empty(args):
+# check if all arguments are empty
+def all_args_are_empty(args:dict):
   for arg in args:
     if args[arg]:
       return False
   return True
     
+# check if only are one argument
 def only_one_arg(x:str, args:dict):
   
   flag = False
