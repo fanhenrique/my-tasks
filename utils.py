cfa975976c87cc92e92f7ed5d96dc9fa128869e9
Henrique Fan <fanhenrique@gmail.com>
2023-06-05 14:11:58 -0300

    count date is no longer here

--- utils.py
+++ utils.py
@@ -7,31 +7,6 @@ from tree.board import Board
 HEADER_CSV = ('node', 'id', 'date', 'star', 'text', 'children', 'check', 'started', 'priority')
 
 
-
-count = 1
-
-def count_date(date):
-    diference = dt.datetime.now().timestamp() - date
-    
-    if diference < 60:
-      return 'now'
-    elif diference >= 60 and diference < 3600:
-      time = int(diference/60)
-      return str(time)+'min'
-    elif diference >= 3600 and diference < 86400:
-      time = int(diference/3600)
-      return str(time)+'h'
-    elif diference >= 86400 and diference < 604800:
-      time = int(diference/86400)
-      return str(time)+'d'
-    elif diference >= 604800 and diference < 31536000:
-      time = int(diference/604800)
-      return str(time)+'week'
-    elif diference >= 31536000:
-      time = int(diference/31536000)
-      return str(time)+'year'
-
-
 def string_type_node(node, first_upcase=False):
   if isinstance(node, Note):
     return 'Note' if first_upcase else 'note'
